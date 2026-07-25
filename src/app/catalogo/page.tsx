56b11e6bef44fe56054520eb448e988850faa117
tallestgabs <gabrielcastrodias1414@gmail.com>
2026-07-25 11:04:45 -0300

    adiciona db no catalogo

--- src/app/catalogo/page.tsx
+++ src/app/catalogo/page.tsx
@@ -1,10 +1,14 @@
-"use client";
-
 import Link from "next/link";
+import Image from "next/image";
 import { Navbar } from "~/_components/Navbar";
 import { Footer } from "~/_components/Footer";
+import { prisma } from "~/lib/prisma";
 
-export default function CatalogoPage() {
+// async para poder esperar a resposta do banco
+export default async function CatalogoPage() {
+  // next.js busca no sqlite antes de renderizar a tela
+  const produtos = await prisma.produto.findMany();
+
   return (
     <main className="--background min-h-screen">
       <Navbar />
@@ -14,6 +18,24 @@ export default function CatalogoPage() {
           Catalogo Page
           <span className="text-green-400">{"}"}</span>
         </h1>
+      </div>
+
+      {/* Listagem dos produtos */}
+      <div>
+        {produtos.map((produto) => (
+          // cada produto fica dentro de uma div simples
+          <div key={produto.id}>
+            <img src={produto.imagemUrl || ""} alt={produto.nome} width={200} />
+
+            <h2>{produto.nome}</h2>
+
+            <p>Valor: R$ {produto.preco}</p>
+
+            <button>Comprar</button>
+
+            <hr />
+          </div>
+        ))}
       </div>
 
       <Footer />
