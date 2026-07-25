a623a53979ae87343c1f713ac1c6e19a058fe949
tallestgabs <gabrielcastrodias1414@gmail.com>
2026-07-25 11:04:23 -0300

    passa adaptador no prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,16 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
+// tipagem de seguranca para o objeto global do Node
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+// adaptador
+const adapter = new PrismaBetterSqlite3({ url: "file:./dev.db" });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// pega prisma na memoria global ou cria um novo passando o adaptador
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+// se nao estiver em producao, salva na memoria
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
